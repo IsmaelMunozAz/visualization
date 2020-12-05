452995d37f1a93ac347296e8190ea3c2814b3c5b
JRuedas <11670560+JRuedas@users.noreply.github.com>
2020-12-05 11:27:40 +0100

    Adds authors and library

--- lesson1/app.R
+++ lesson1/app.R
@@ -1,2 +1,26 @@
-library(shiny)
+# Authors
+# José Domínguez Pérez
+# Ismael Muñoz Aztout
+# Jonatan Ruedas Mora
+
+# Libraries
+
+## If a package is installed, it will be loaded. If any
+## are not, the missing package(s) will be installed
+## from CRAN and then loaded.
+
+## First specify the packages of interest
+packages = c("shiny")
+
+## Now load or install & load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
 runExample("01_hello")
