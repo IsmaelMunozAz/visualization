1184dbe51e89f32874ebbe68c286b31b6ad9eaa8
ismael.munoz <izzmael95@gmail.com>
2020-12-06 09:59:48 +0100

    lesson5

--- census/App.R
+++ census/App.R
@@ -1,45 +1,27 @@
-# Authors
-# José Domínguez Pérez
-# Ismael Muñoz Aztout
-# Jonatan Ruedas Mora
-
-# Libraries
-
-## If a package is installed, it will be loaded. If any
-## are not, the missing package(s) will be installed
-## from CRAN and then loaded.
-
-## First specify the packages of interest
-packages = c("shiny")
-
-## Now load or install & load all
-package.check <- lapply(
-  packages,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-)
-
+# Load packages ----
 library(shiny)
+library(maps)
+library(mapproj)
 
-# Define UI ----
+# Load data ----
+counties <- readRDS("data/counties.rds")
+
+# Source helper functions -----
+source("helpers.R")
+
+# User interface ----
 ui <- fluidPage(
   titlePanel("censusVis"),
   
   sidebarLayout(
     sidebarPanel(
       helpText("Create demographic maps with 
-               information from the 2010 US Census."),
+        information from the 2010 US Census."),
       
       selectInput("var", 
                   label = "Choose a variable to display",
-                  choices = c("Percent White", 
-                              "Percent Black",
-                              "Percent Hispanic", 
-                              "Percent Asian"),
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
                   selected = "Percent White"),
       
       sliderInput("range", 
@@ -47,24 +29,24 @@ ui <- fluidPage(
                   min = 0, max = 100, value = c(0, 100))
     ),
     
-    mainPanel(
-      textOutput("selected_var"),
-      textOutput("selected_range")
-    )
+    mainPanel(plotOutput("map"))
   )
 )
 
-# Define server logic ----
 server <- function(input, output) {
-  
-  output$selected_var <- renderText({ 
-    paste("You have selected", input$var)
-  })
-  
-  output$selected_range <- renderText({ 
-    paste("You have chosen a range that goes from ", input$range[1], " to ", input$range[2])
+  output$map <- renderPlot({
+    args <- switch(input$var,
+                   "Percent White" = list(counties$white, "darkgreen", "% White"),
+                   "Percent Black" = list(counties$black, "black", "% Black"),
+                   "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+                   "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+    
+    args$min <- input$range[1]
+    args$max <- input$range[2]
+    
+    do.call(percent_map, args)
   })
 }
 
 # Run the app ----
-shinyApp(ui = ui, server = server) 
+shinyApp(ui = ui, server = server)
